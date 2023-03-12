7bcc698a892c7dfa98e7674c6bd8d10c8b1b41cb
Timothy <timothy@Zipcoders-MBP-3.fios-router.home>
2023-03-12 17:16:23 -0400

    added changes

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,28 @@
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+    int max = 6;
+    //int lowestPossibleNumber = numOfDies;
+    int timesThrown = 0;
+    int numOfDice = 0;
+
+    public Dice(int numOfDice) {
+        this.numOfDice = numOfDice;
+    }
+
+    public int tossAndSum() {
+        int result = 0;
+        int ran = 0;
+        Random random = new Random();
+        for (int i = 0; i < numOfDice; i++) {
+            ran = random.nextInt(6) + 1;
+            result += ran;
+        }
+        return result;
+    }
+
+    public int randomDieResult() {
+        return 0;
+    }
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Simulation {
+    private int numDie;
+    private int numTosses;
+    Dice dice;
 
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation(2, 1000000);
+        simulation.runSimulation();
+        simulation.printResults();
+    }
 
+    public Simulation(int numDie, int numTosses) {
+        dice = new Dice(numDie);
+        this.numDie = numDie;
+        this.numTosses = numTosses;
+    }
+
+    public void runSimulation() {
+        for (int i = 1; i <= numTosses; i++) {
+            dice.tossAndSum();
+        }
+    }
+
+    public void printResults() {
+
+    }
 
 }
